938f2592e239cfc8123898f9ed7a049b6e37410b
Казаков Виктор Андреевич <ViAKazakov@vimpelcom.ru>
2024-02-02 23:46:25 +0300

    First CI push

---
+++ config/links.py
@@ -0,0 +1,7 @@
+
+
+class Links:
+    HOST = "https://nsm-tst.beeline.ru/ux"
+    NSM = "https://nsm-tst.beeline.ru/ux"
+    MY_IT = f"{HOST}/smart-it/"
+    PROBLEM = f"{HOST}/smart-it/#/create/problem"
